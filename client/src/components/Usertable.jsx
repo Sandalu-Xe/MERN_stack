5ac73b86938313018fa846e02bbe200f92b78984
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-11-05 10:51:50 +0530

    add changes

--- client/src/components/Usertable.jsx
+++ client/src/components/Usertable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+
 import { Table } from 'react-bootstrap';
 
 const Usertable = () => {
